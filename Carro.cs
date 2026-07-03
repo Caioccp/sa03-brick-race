4ebafdd4c2687993f0ddbc22746d8c2558d167bd
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-07-02 21:03:55 -0300

    Foguete 1.1.5

---
+++ Carro.cs
@@ -0,0 +1,69 @@
+using System;
+
+public class Foguete
+{
+    // 0 = pista esquerda
+    // 1 = pista direita
+    public int Pista { get; private set; }
+
+    public Foguete()
+    {
+        // O foguete começa na pista da esquerda
+        Pista = 0;
+    }
+
+    // Move para a pista da esquerda
+    public void MoverEsquerda()
+    {
+        Pista = 0;
+    }
+
+    // Move para a pista da direita
+    public void MoverDireita()
+    {
+        Pista = 1;
+    }
+
+    // Lê as teclas do jogador
+    public void LerTecla(ConsoleKey tecla)
+    {
+        if (tecla == ConsoleKey.A || tecla == ConsoleKey.LeftArrow)
+        {
+            MoverEsquerda();
+        }
+        else if (tecla == ConsoleKey.D || tecla == ConsoleKey.RightArrow)
+        {
+            MoverDireita();
+        }
+    }
+
+    // Desenha o foguete
+    public void Desenhar()
+    {
+        int coluna;
+        int linha = 5;
+
+        // Escolhe a pista
+        if (Pista == 0)
+        {
+            coluna = 13;
+        }
+        else
+        {
+            coluna = 20;
+        }
+
+        Console.SetCursorPosition(coluna, linha);
+        Console.Write(" ▲ ");
+
+        Console.SetCursorPosition(coluna, linha + 1);
+        Console.Write("/█\\");
+
+        Console.SetCursorPosition(coluna, linha + 2);
+        Console.Write("/_\\");
+
+        Console.SetCursorPosition(coluna, linha + 3);
+        Console.Write(" | ");
+
+    }
+}
