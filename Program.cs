74e1880ec7cf5a42df83bf394e30afead1b68c16
Caio <caio_pouzada@estudante.sesisenai.org.br>
2026-07-06 19:59:54 -0300

    Resolve conflito no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-﻿Console.Beep(1000, 500);
-
-Menu menu = new Menu();
-menu.Exibir();
-=======
-﻿
 using System;
 using System.Text;
 
@@ -14,12 +7,12 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
-            Console.WindowHeight = 30; 
-            Console.WindowWidth = 80;
-            Console.BufferHeight = 200;
-            Console.BufferWidth = 100;
+        Console.WindowHeight = 30;
+        Console.WindowWidth = 80;
+        Console.BufferHeight = 200;
+        Console.BufferWidth = 100;
+
         Menu menu = new Menu();
         menu.Exibir();
     }
 }
->>>>>>> 2a6c11eb00e6c185bd666c986ccc9f89e1eb45c7
