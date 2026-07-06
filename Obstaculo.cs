b931809fb2e2a88e8af1d108af4a62204d154256
Caio <caio_pouzada@estudante.sesisenai.org.br>
2026-07-06 19:23:39 -0300

    Obstaculos

--- Obstaculo.cs
+++ Obstaculo.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 
 public class Obstaculo
 {
-    // Linha, pista e tipo do obstáculo
     public List<(int Linha, int Pista, int Tipo)> Obstaculos { get; private set; }
 
     private Random random = new Random();
 
     private int alturaTela;
 
-    // Não repetir o mesmo obstáculo
     private int ultimoTipo = -1;
 
-    // Alternar entre as pistas
     private int ultimaPista = -1;
 
     public Obstaculo(int alturaTela)
@@ -22,17 +19,14 @@ public class Obstaculo
         Obstaculos = new List<(int, int, int)>();
     }
 
-    // Cria um novo obstáculo
     public void CriarObstaculo()
     {
-        // Só cria outro quando o último já desceu um pouco
         foreach (var obstaculo in Obstaculos)
         {
             if (obstaculo.Linha < 5)
                 return;
         }
 
-        // Alterna as pistas para nunca bloquear as duas
         int pista;
 
         do
@@ -43,7 +37,6 @@ public class Obstaculo
 
         ultimaPista = pista;
 
-        // Escolhe um obstáculo diferente do último
         int tipo;
 
         do
@@ -57,7 +50,6 @@ public class Obstaculo
         Obstaculos.Add((0, pista, tipo));
     }
 
-    // Faz os obstáculos descerem
     public void Atualizar()
     {
         for (int i = 0; i < Obstaculos.Count; i++)
@@ -70,11 +62,9 @@ public class Obstaculo
             );
         }
 
-        // Remove quando sair da tela
         Obstaculos.RemoveAll(o => o.Linha > alturaTela);
     }
 
-    // Desenha os obstáculos
     public void Desenhar()
     {
         foreach (var obstaculo in Obstaculos)
@@ -85,22 +75,18 @@ public class Obstaculo
 
             switch (obstaculo.Tipo)
             {
-                // Meteoro
                 case 0:
                     Console.Write("[☄]");
                     break;
 
-                // Nave alienígena
                 case 1:
                     Console.Write("<^>");
                     break;
 
-                // Estrela
                 case 2:
                     Console.Write("<✦>");
                     break;
 
-                // Buraco negro
                 case 3:
                     Console.Write("(◉)");
                     break;
