2f35f0bf1dc3e38122908e44b4b70794673e5172
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-07-06 19:59:52 -0300

    arruando

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-﻿Console.Beep(1000, 500);
-
-Menu menu = new Menu();
-menu.Exibir();
-=======
-﻿
 using System;
 using System.Text;
 
@@ -22,4 +15,3 @@ class Program
         menu.Exibir();
     }
 }
->>>>>>> 2a6c11eb00e6c185bd666c986ccc9f89e1eb45c7
