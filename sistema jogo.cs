f94811b53df4e7def3ed2591bb4e1f4d83e7a663
Caua <caua_schuh@estudante.sesisenai.org.br>
2026-07-06 19:18:43 -0300

    velocidade e nivel atualizados

--- sistema jogo.cs
+++ sistema jogo.cs
@@ -61,45 +61,57 @@ if (Vidas == 0)
     }
 
     public void AtualizarNivel()
-    {
-        if (Pontos >= 300)
-            Nivel = 4;
-        else if (Pontos >= 200)
-            Nivel = 3;
-        else if (Pontos >= 100)
-            Nivel = 2;
-        else
-            Nivel = 1;
-
-            if (Nivel > nivelAnterior)
 {
-    Console.Beep(700, 100);
-    Console.Beep(900, 100);
-    Console.Beep(1100, 200);
-}
+    int nivelAnterior = Nivel;
+
+    if (Pontos >= 900)
+        Nivel = 10;
+    else if (Pontos >= 800)
+        Nivel = 9;
+    else if (Pontos >= 700)
+        Nivel = 8;
+    else if (Pontos >= 600)
+        Nivel = 7;
+    else if (Pontos >= 500)
+        Nivel = 6;
+    else if (Pontos >= 400)
+        Nivel = 5;
+    else if (Pontos >= 300)
+        Nivel = 4;
+    else if (Pontos >= 200)
+        Nivel = 3;
+    else if (Pontos >= 100)
+        Nivel = 2;
+    else
+        Nivel = 1;
+
+    if (Nivel > nivelAnterior)
+    {
+        Console.Beep(700, 100);
+        Console.Beep(900, 100);
+        Console.Beep(1100, 200);
     }
+}
+
+    {
 
     public void AtualizarVelocidade()
+{
+    switch (Nivel)
     {
-        switch (Nivel)
-        {
-            case 1:
-                Velocidade = 300;
-                break;
-
-            case 2:
-                Velocidade = 250;
-                break;
-
-            case 3:
-                Velocidade = 200;
-                break;
-
-            default:
-                Velocidade = 150;
-                break;
-        }
+        case 1: Velocidade = 300; break;
+        case 2: Velocidade = 280; break;
+        case 3: Velocidade = 260; break;
+        case 4: Velocidade = 240; break;
+        case 5: Velocidade = 220; break;
+        case 6: Velocidade = 200; break;
+        case 7: Velocidade = 180; break;
+        case 8: Velocidade = 160; break;
+        case 9: Velocidade = 140; break;
+        case 10: Velocidade = 120; break;
+        default: Velocidade = 120; break;
     }
+}
 
     public void SalvarResultado()
     {
