52bc864a7038270e2da49dbea7a67df923865818
Caua <caua_schuh@estudante.sesisenai.org.br>
2026-07-13 19:24:59 -0300

    mais niveis

--- SistemasJogo.cs
+++ SistemasJogo.cs
@@ -57,30 +57,50 @@ public class SistemaJogo
     }
 
     public void AtualizarNivel()
-    {
-        if (Pontos >= 900)
-            Nivel = 10;
-        else if (Pontos >= 800)
-            Nivel = 9;
-        else if (Pontos >= 700)
-            Nivel = 8;
-        else if (Pontos >= 600)
-            Nivel = 7;
-        else if (Pontos >= 500)
-            Nivel = 6;
-        else if (Pontos >= 400)
-            Nivel = 5;
-        else if (Pontos >= 300)
-            Nivel = 4;
-        else if (Pontos >= 200)
-            Nivel = 3;
-        else if (Pontos >= 100)
-            Nivel = 2;
-        else
-            Nivel = 1;
+{
+    if (Pontos >= 1900)
+        Nivel = 20;
+    else if (Pontos >= 1800)
+        Nivel = 19;
+    else if (Pontos >= 1700)
+        Nivel = 18;
+    else if (Pontos >= 1600)
+        Nivel = 17;
+    else if (Pontos >= 1500)
+        Nivel = 16;
+    else if (Pontos >= 1400)
+        Nivel = 15;
+    else if (Pontos >= 1300)
+        Nivel = 14;
+    else if (Pontos >= 1200)
+        Nivel = 13;
+    else if (Pontos >= 1100)
+        Nivel = 12;
+    else if (Pontos >= 1000)
+        Nivel = 11;
+    else if (Pontos >= 900)
+        Nivel = 10;
+    else if (Pontos >= 800)
+        Nivel = 9;
+    else if (Pontos >= 700)
+        Nivel = 8;
+    else if (Pontos >= 600)
+        Nivel = 7;
+    else if (Pontos >= 500)
+        Nivel = 6;
+    else if (Pontos >= 400)
+        Nivel = 5;
+    else if (Pontos >= 300)
+        Nivel = 4;
+    else if (Pontos >= 200)
+        Nivel = 3;
+    else if (Pontos >= 100)
+        Nivel = 2;
+    else
+        Nivel = 1;
 
-        nivelAnterior = Nivel;
-    }
+    nivelAnterior = Nivel;
+}
 
     public void AtualizarVelocidade()
     {
